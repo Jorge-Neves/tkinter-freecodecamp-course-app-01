a57ef3fe6b661ae8392ee9a4f5f95ebba907a294
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-11-19 13:09:02 +0000

    Add main loop

--- Inttro.py
+++ Inttro.py
@@ -1,8 +1,13 @@
 from tkinter import *
 
-# Root
+# Root Widget
 root = Tk()
 
 # Label Widget
 myLabel = Label(root, text="Hello GitHub")
 
+# Packing the widget into the screen
+myLabel.pack()
+
+# Main loop
+root.mainloop()
